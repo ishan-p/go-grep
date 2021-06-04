5f373d9f1dd6c7380b88c63503378e6d2cc2e194
Ishan <ishan@one2n.in>
2021-06-04 14:52:58 +0530

    Initial commit
    Add Grep function to accept input file and a search query.
    Output all occurences of the search query in the given file

---
+++ grep/grep.go
@@ -0,0 +1,56 @@
+package gogrep
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func Grep(searchQuery string, inputFile string) {
+	fileHandler, err := os.Open(inputFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fileHandler.Close()
+	fileBuffCh := make(chan string)
+	go readFileByLine(fileHandler, fileBuffCh)
+	for text := range fileBuffCh {
+		searchResult := Find(searchQuery, text)
+		if len(searchResult) > 0 {
+			fmt.Printf("%s\n", searchResult)
+		}
+	}
+	fmt.Println(string("\033[31m"), "test")
+}
+
+func readFileByLine(fileHandler *os.File, channel chan string) {
+	scanner := bufio.NewScanner(fileHandler)
+
+	for scanner.Scan() {
+
+		channel <- scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	close(channel)
+}
+
+func Find(searchQuery string, text string) []byte {
+	re := regexp.MustCompile(searchQuery)
+	input := []byte(text)
+	result := re.FindAll([]byte(text), -1)
+	var output []byte
+	if len(result) > 0 {
+		for _, val := range result {
+			coloredOp := fmt.Sprintf("\033[31m%s\033[0m", val)
+			output = re.ReplaceAll(input, []byte(coloredOp))
+		}
+	}
+	return output
+}
